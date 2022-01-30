814879f8ae1324ad854a38b1454cc129b4516772
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-01-29 22:42:50 -0300

    Version 1.0.2

--- test.js
+++ test.js
@@ -2,4 +2,6 @@ const cliprxyz = require("./index")
 
 cliprxyz.downloadClip("https://clips.twitch.tv/SuaveDistinctLlamaPraiseIt-33EJqtw6dJMDbIIg").then(res => {
     console.log(res)
+}).catch(err => {
+  console.log(err);
 });
